9a185b9da2bcdc16eea57a5da5e93fcb5852001b
Krzysztof Rećko <yetioszek@gmail.com>
2015-03-03 11:30:18 +0100

    Better bounding of chunk.

--- source/blender/blenkernel/intern/mball_tessellate.c
+++ source/blender/blenkernel/intern/mball_tessellate.c
@@ -142,7 +142,10 @@ typedef struct process {        /* parameters, storage */
 
 typedef struct chunk {
 	PROCESS *process;
+
 	Box bb;
+	int min_lat[3], max_lat[3];
+
 	MetaElem **mainb;
 	unsigned int elem;
 
@@ -1014,13 +1017,9 @@ static void add_cube(CHUNK *chunk, int i, int j, int k)
 	CUBES *ncube;
 	int n;
 
-	int max[3], min[3];
-	next_lattice(max, chunk->bb.max, chunk->process->size);
-	next_lattice(min, chunk->bb.min, chunk->process->size);
-
-	if (i < min[0] || i > max[0] ||
-		j < min[1] || j > max[1] ||
-		k < min[2] || k > max[2]) return;
+	if (i <= chunk->min_lat[0] || i >= chunk->max_lat[0] ||
+		j <= chunk->min_lat[1] || j >= chunk->max_lat[1] ||
+		k <= chunk->min_lat[2] || k >= chunk->max_lat[2]) return;
 
 	/* test if cube has been found before */
 	if (setcenter(chunk, chunk->centers, i, j, k) == 0) {
@@ -1161,6 +1160,9 @@ static void polygonize(PROCESS *process)
 
 		chunks[i].bb.max[1] = process->allbb.min[1] + step * (i + 1);
 		chunks[i].bb.min[1] = process->allbb.min[1] + step * i;
+
+		next_lattice(chunks[i].max_lat, chunks[i].bb.max, process->size);
+		prev_lattice(chunks[i].min_lat, chunks[i].bb.min, process->size);
 	}
 	
 #pragma omp parallel for
